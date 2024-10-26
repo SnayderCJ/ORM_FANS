932f80411e01e4fa755634690f7e1bb604f75fd7
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-10-26 17:12:44 -0500

    Admin && BD

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Libro)
+admin.site.register(Autor)
+admin.site.register(AutorCapitulo)
+admin.site.register(Editorial)
+admin.site.register(LibroCronica)
+admin.site.register(VLibroautores)
